3f527e77119f49d4d0ec5bda3e4f91fa199208a8
Corey Edwards <ensabahnur16@gmail.com>
2015-10-07 10:05:06 -0500

    Nein!!
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- device.mk
+++ device.mk
@@ -17,28 +17,7 @@
 # This file includes all definitions that apply to ALL hammerhead devices, and
 # are also specific to hammerhead devices
 #
-# Everything in this directory will become public
-
-
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-ifeq ($(USE_SVELTE_KERNEL),true)
-LOCAL_KERNEL := device/lge/hammerhead_svelte-kernel/zImage-dtb
-else
-
-ifneq ($(filter hammerhead_fp aosp_hammerhead_fp,$(TARGET_PRODUCT)),)
-LOCAL_KERNEL := device/lge/hammerhead_fp-kernel/zImage-dtb
-else
-LOCAL_KERNEL := device/lge/hammerhead-kernel/zImage-dtb
-endif
-
-endif
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-
-PRODUCT_COPY_FILES := \
-    $(LOCAL_KERNEL):kernel
+# Everything in this directory will become publics
 
 PRODUCT_COPY_FILES += \
     device/lge/hammerhead/init.hammerhead.rc:root/init.hammerhead.rc \
